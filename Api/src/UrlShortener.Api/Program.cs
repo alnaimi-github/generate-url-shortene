4b72af24deb291c800be21e4175457388c412b15
alnaimi-github <alnymybdalwas1@gmail.com>
2024-12-31 00:45:23 +0300

    Remove obsolete test files and add new API implementation with weather forecast endpoint

---
+++ Api/src/UrlShortener.Api/Program.cs
@@ -0,0 +1,48 @@
+using Azure.Identity;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var keyVaultName = builder.Configuration["KeyVaultName"];
+if(!string.IsNullOrEmpty(keyVaultName))
+{
+    builder.Configuration.AddAzureKeyVault(
+        new Uri($"https://{keyVaultName}.vault.azure.net/"),
+        new DefaultAzureCredential());
+}
+
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
---
+++ Api/tests/UrlShortener.Api.Core.Tests/Base62EncodingScenarios.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+
+namespace UrlShortener.Api.Core.Tests;
+
+public class Base62EncodingScenarios
+{
+    [Theory]
+    [InlineData(0,"0")]
+    [InlineData(1,"1")]
+    [InlineData(20,"k")]
+    [InlineData(62,"10")]
+    public void Should_encode_number_to_base62(long number,string expected)
+    {
+        number.EncodeToBase62()
+            .Should()
+            .Be(expected);
+    }
+}
